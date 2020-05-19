db0608ca7c3f9db484b37fd3ff5c4f890c36eb0f
Jacob Flora <jacobflora4@gmail.com>
2020-05-19 12:28:46 -0400

    js code is almost complete. working out the bugs.

--- js/main.js
+++ js/main.js
@@ -1,33 +1,58 @@
-// url        Example for rainbow search: http://api.giphy.com/v1/gifs/search?api_key=9KemuYKuM7DozeEmiGoMHyPT9qqCBgdO&q=rainbow
+// Load DOM before scripts.
 
-const apiKey = "9KemuYKuM7DozeEmiGoMHyPT9qqCBgdO";
+document.addEventListener("DOMContentLoaded", function(event) { 
+  
+    // url        Example for rainbow search: http://api.giphy.com/v1/gifs/search?api_key=9KemuYKuM7DozeEmiGoMHyPT9qqCBgdO&q=rainbow
 
-const url = `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=`;
+    const apiKey = "9KemuYKuM7DozeEmiGoMHyPT9qqCBgdO";
+    const url = `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=`;
 
-// Selecting DOM elements.
-const submitElement = document.querySelector('.js-btn');
-const inputElement = document.querySelector('.js-input');
-const containerElement = document.querySelector('.js-container');
+    // Selecting DOM elements.
+    const submitElement = document.querySelector('.js-form');
+    const inputElement = document.querySelector('.js-input');
+    const container = document.querySelector('.js-container');
+    const searchIcon = document.querySelector('.js-icon');
 
-// Event listener.
-submitElement.addEventListener('click', function(e) {
-    // Prevent default page reload.
-    e.preventDefault();
+    let inputValue = '';
 
-    // Create input value variable and dynamic url.
-    const value = inputElement.value;
-    const newUrl = url + value;
+    function convertResponseToJson( response ) {
+        return response.json();
+    }
 
-    // Fetching URL
-    fetch(newUrl)
+    /* data/images/original/url */
+    function processJsonResponse( data ) {
+        let markup = `<li>${inputValue}</li>`;
+        for (const images of data) {
+            markup = markup + `
+            <li>${images.original.url}
+            `;
+        }
+        container.innerHTML = markup;
+    }
+    // Error handling
+    function errorHandling() {
+        container.innerHTML = `
+        <li class="error">Error loading data. Make sure your network is on.</li>
+        `;
+    }
 
-        // Return promise, take response and convert to JSON.
-        .then((res) => res.json())
+    // Event Listener
+    submitElement.addEventListener('submit', loadData);
+    searchIcon.addEventListener('click', loadData);
 
-        // Turning JSON data and turning it into a variable.
-        .then((data) => {
-            //let //Gif? = data.results;
-            //let //output? = '';
-        });
-    });
-
+    function loadData(event) {
+        // Retrieving User Input
+        const value = inputElement.value;
+        // Creating New URL by concantinating API URL and user input.
+        const newUrl = url + value;
+        // Prevent Page from reloading
+        event.preventDefault();
+        // Inputing data objects from JSON data array.
+        inputValue = document.querySelector('[data=url]').value;
+        // Fetching the concantinated URL.
+        fetch(newUrl)
+            .then( convertResponseToJson )
+            .then( processJsonResponse )
+            .catch( errorHandling );
+    }
+});
