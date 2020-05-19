582958346567a47ca39968eafce23a54ed8f9ad1
Jacob Flora <jacobflora4@gmail.com>
2020-05-19 19:49:48 -0400

    created dynamic url with search input and quantity input

--- js/main.js
+++ js/main.js
@@ -2,26 +2,30 @@
 
 document.addEventListener("DOMContentLoaded", function(event) { 
   
-    // url        Example for rainbow search: http://api.giphy.com/v1/gifs/search?api_key=9KemuYKuM7DozeEmiGoMHyPT9qqCBgdO&q=rainbow
+    // url        JSON data for a rainbow search: http://api.giphy.com/v1/gifs/search?api_key=9KemuYKuM7DozeEmiGoMHyPT9qqCBgdO&q=rainbow&limit=5
 
     const apiKey = "9KemuYKuM7DozeEmiGoMHyPT9qqCBgdO";
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=`;
+    const url = `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}`;
 
     // Selecting DOM elements.
-    const submitElement = document.querySelector('.js-form');
-    const inputElement = document.querySelector('.js-input');
-    const inputNumElement = document.querySelector('.js-input-num');
+    const submit = document.querySelector('.js-form');
+
+    /* input variables */
+    const inputSearch = document.querySelector('.js-input');
+    const inputNum = document.querySelector('.js-input-num');
     const container = document.querySelector('.js-container');
     const searchIcon = document.querySelector('.js-icon');
 
     let inputValue = '';
 
     function convertResponseToJson( response ) {
+
         return response.json();
     }
 
     /* data/images/original/url */
     function processJsonResponse( data ) {
+        console.log(data);
         let markup = `<li>${inputValue}</li>`;
         for (const images of data) {
             markup = markup + `
@@ -29,6 +33,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
             `;
         }
         container.innerHTML = markup;
+
+     
     }
     // Error handling
     function errorHandling() {
@@ -37,25 +43,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
         `;
     }
 
+    // Event Listener
+    submit.addEventListener('submit', loadData);
+    searchIcon.addEventListener('click', loadData);
     
-
     function loadData(event) {
         // Retrieving User Input
-        const value = inputElement.value;
+        const value = ('&q=' + inputSearch.value) + ('&limit=' + inputNum.value);
         // Creating New URL by concantinating API URL and user input.
         const newUrl = url + value;
         // Prevent Page from reloading
         event.preventDefault();
         // Inputing data objects from JSON data array.
-        inputValue = document.querySelector('[data]').value;
+        inputValue = document.querySelector('.js-container').value;
         // Fetching the concantinated URL.
         fetch(newUrl)
-            .then( convertResponseToJson )
-            .then( processJsonResponse )
-            .catch( errorHandling );
-    }
-
-    // Event Listener
-    submitElement.addEventListener('submit', loadData);
-    searchIcon.addEventListener('click', loadData);
+            .then(processJsonResponse)
+            .then(convertResponseToJson)
+            .catch(errorHandling);
+        console.log('Dynamic URL is ',newUrl)
+    }    
 });
